7c2b4f9e1d67044d670ee17bdb7b0535473af73a
BartoszMilanowski <bartosz.milanowski@protonmail.com>
2024-02-11 15:13:44 +0100

    skip video for- and backward

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ const customControls = () => {
     const video = container.querySelector('video');
     const progressBar = container.querySelector('.progress-bar');
     const playPauseBtn = container.querySelector('.play-pause i');
+    const skipBackward = container.querySelector('.skip-backward i');
+    const skipForward = container.querySelector('.skip-forward i');
 
+    //Move progress bar
     video.addEventListener('timeupdate', e => {
         let { currentTime, duration } = e.target;
-        let percent = (currentTime /  duration) * 100;
+        let percent = (currentTime / duration) * 100;
         progressBar.style.width = `${percent}%`;
     })
 
@@ -33,13 +36,28 @@ const customControls = () => {
         video.paused ? video.play() : video.pause();
     }
 
+    //Skip video
+    const skipBackwardFunc = () => video.currentTime -= 5;
+    const skipForwardFunc = () => video.currentTime += 5;
+
+    skipBackward.addEventListener('click', () => skipBackwardFunc());
+    skipForward.addEventListener('click', () => skipForwardFunc());
+
     //Proceed by keyboard
     document.addEventListener('keydown', (event) => {
         switch (event.code) {
             case 'Space':
                 playPauseVid();
+                break;
+            case 'ArrowLeft':
+                skipBackwardFunc();
+                break;
+            case 'ArrowRight':
+                skipForwardFunc();
+                break;
             default:
                 '';
+                break;
         }
     })
 
