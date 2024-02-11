839bf6f436541e48f714259ff9592a361a62f1d8
BartoszMilanowski <bartosz.milanowski@protonmail.com>
2024-02-11 14:53:23 +0100

    add playPause with click on video & space key

--- script.js
+++ script.js
@@ -3,26 +3,38 @@ document.addEventListener('DOMContentLoaded', () => {
     selectFromList();
 })
 
-const customControls = () =>{
+const customControls = () => {
 
     const container = document.querySelector('.video-container');
     const video = container.querySelector('video');
+    const progressBar = container.querySelector('.progress-bar');
     const playPauseBtn = container.querySelector('.play-pause i');
 
-    playPauseBtn.addEventListener('click', () => {
-        video.paused ? video.play() : video.pause();
-    })
-    
-    video.addEventListener('click', () => {
-        video.paused ? video.play() : video.pause();
-    })
+
+    //Play-pause proceed
+    playPauseBtn.addEventListener('click', () => playPauseVid());
+    video.addEventListener('click', () => playPauseVid());
 
     video.addEventListener('play', () => {
         playPauseBtn.classList.replace('fa-play', 'fa-pause');
-    })
-    
+    });
+
     video.addEventListener('pause', () => {
         playPauseBtn.classList.replace('fa-pause', 'fa-play');
+    });
+
+    const playPauseVid = () => {
+        video.paused ? video.play() : video.pause();
+    }
+
+    //Proceed by keyboard
+    document.addEventListener('keydown', (event) => {
+        switch (event.code) {
+            case 'Space':
+                playPauseVid();
+            default:
+                '';
+        }
     })
 
 };
@@ -31,12 +43,12 @@ const selectFromList = () => {
     let listVideo = document.querySelectorAll('.video-list .vid');
     let mainVideo = document.querySelector('.main-video video');
     let title = document.querySelector('.main-video .title');
-    
+
     listVideo.forEach(video => {
         video.onclick = () => {
             listVideo.forEach(vid => vid.classList.remove('active'));
             video.classList.add('active');
-            if(video.classList.contains('active')){
+            if (video.classList.contains('active')) {
                 let src = video.children[0].getAttribute('src');
                 mainVideo.src = src;
                 let text = video.children[1].innerHTML;
